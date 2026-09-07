d3106dd873ab197a76b2d082f08a491007901419
Arslan <arsalana935@gmail.comgit config --global user.user.name Arslan660git config --global user.email arsalana935@gmail.com>
2026-09-07 19:51:05 +0500

    Initial Node.js app

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const AWS = require('aws-sdk');
+const app = express();
+const PORT = 3000;
+
+const s3 = new AWS.S3();
+const BUCKET_NAME = 'your-bucket-name-here'; // baad mein update karoge
+
+app.get('/', (req, res) => {
+  res.send('<h1>Node.js App Running on EC2!</h1><p>Hostname: ' + require('os').hostname() + '</p>');
+});
+
+app.get('/list-files', async (req, res) => {
+  try {
+    const data = await s3.listObjectsV2({ Bucket: BUCKET_NAME }).promise();
+    res.json(data.Contents);
+  } catch (err) {
+    res.status(500).send('Error: ' + err.message);
+  }
+});
+
+app.listen(PORT, () => console.log(`App running on port ${PORT}`));
